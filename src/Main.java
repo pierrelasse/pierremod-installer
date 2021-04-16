6c9b0926944786c2245b0ba33bd244070f39fe14
pierrelasse <paab20@web.de>
2021-04-16 11:49:05 +0200

    #

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String appdata = System.getProperty("user.home") ;
+        System.out.println("appdata: "  + appdata);
+
+
+	    System.out.println("Test");
+    }
+}
